9c90b51bffad3211fbb70b7fa4d312d2726ee0a7
Joel Clark <jc214809@ohio.edu>
2025-07-05 16:09:23 -0400

    Add DST logic to pytz stub and tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -25,3 +25,7 @@ def test_deep_update():
 def test_get_eastern():
     eastern_time = get_eastern("2024-01-01T12:00:00Z")
     assert eastern_time.endswith("07:00 AM")
+
+    # During daylight saving time the offset should be UTC-4
+    dst_time = get_eastern("2024-07-01T12:00:00Z")
+    assert dst_time.endswith("08:00 AM")
